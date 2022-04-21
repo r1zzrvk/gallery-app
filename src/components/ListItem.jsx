732bffbd17508b8effc02bab5fa51c14014d7a2c
Stanislav <r1zzrvk@gmail.com>
2022-04-21 13:43:12 +0300

    hover buttons

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import { Figure } from 'react-bootstrap'
+import { useState } from 'react'
+import { Button, Figure } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
-const ListItem = ({item}) => {
+const ListItem = ({ item }) => {
   const history = useNavigate();
-
+  const [show, setShow] = useState(false);
   return (
-    <Figure className='p-2'>
-      <Figure.Image 
-      src={item.url}
-      width={300}
-      height={200}
-      onClick={() => {
-        history(`/item/${item.id}`)
-      }}
-      />
+    <Figure className='p-2' style={{ position: 'relative' }}>
+      <div
+        onMouseOver={() => { setShow(true) }}
+        onMouseLeave={() => { setShow(false) }}>
+        <Figure.Image
+          src={item.url}
+          width={300}
+          height={200}
+        />
+        <Button
+          onClick={() => {
+            history(`/item/${item.id}`)
+          }}
+          style={{
+            position: 'absolute',
+            top: '70%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            display: show === true ? '' : 'none'
+          }}>Подробнее</Button>
+      </div>
     </Figure>
   )
 }
